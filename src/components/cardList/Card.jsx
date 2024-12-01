bd89ba506258ce7249b49d7f181f4c6f1dedce76
lfemina <lfemina@yandex.ru>
2024-12-01 17:51:28 +0300

    added a button to hide the translation

--- src/components/cardList/Card.jsx
+++ src/components/cardList/Card.jsx
@@ -1,12 +1,21 @@
+import React, { useState } from "react";
 import Buttons from "../buttons/Buttons";
 import "./Card.css";
+import "../buttons/Buttons.css";
 
 const Card = ({ word, transcription, translation, topic }) => {
+
+    const [showTranslation, setShowTranslation] = useState(false);
+
     return (
         <div className="card">
             <h2 className="font_h2">{word}</h2>
             <p className="font_p">Транскрипция: {transcription}</p>
-            <p className="font_p">Перевод: {translation}</p>
+            {showTranslation ? (
+                <p className={`font_p font_translate`}>Перевод: {translation}</p>
+            ) : (
+                <button className="btn btn_translate font_p" onClick={() => setShowTranslation(true)}>Показать перевод</button>
+            )}
             <p className="font_p">Тема: {topic}</p>
             <Buttons />
         </div>
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,13 +1,23 @@
+import React, { useState } from "react";
 import Card from "./Card";
 
-const words = [
-    { word: 'Father', transcription: 'ˈfäT͟Hər', translation: 'Отец', topic: 'Family'},
-    { word: 'Mother', transcription: 'ˈməT͟Hər', translation: 'Мать', topic: 'Family'},
-    { word: 'Brother', transcription: 'ˈbrəT͟Hər', translation: 'Брат', topic: 'Family'},
-    { word: 'Sister', transcription: 'ˈsistər', translation: 'Сестра', topic: 'Family'},
-];
-
 const CardList = ({ style }) => {
+    const [words, setWords] = useState([
+        { word: 'Father', transcription: 'ˈfäT͟Hər', translation: 'Отец', topic: 'Family'},
+        { word: 'Mother', transcription: 'ˈməT͟Hər', translation: 'Мать', topic: 'Family'},
+        { word: 'Brother', transcription: 'ˈbrəT͟Hər', translation: 'Брат', topic: 'Family'},
+        { word: 'Sister', transcription: 'ˈsistər', translation: 'Сестра', topic: 'Family'},
+    ]);
+
+    const [newWord, setNewWord] = useState('');
+    const [newTranscription, stNewTranscription] = useState('');
+    const [newTranslation, setNewTranslation] = useState('');
+    const [newTopic, setNewTopic] = useState('');
+
+    const handleAddWord = () => {
+        const newEntry = {word: newWord, transcription: newTranscription, translation: newTranslation, topic: newTopic }
+    };
+
     return (
         <div className="card-list" style={style}>
             {words.map((wordItem, index) => (
