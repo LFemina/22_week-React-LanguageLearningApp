98472d06ccef0c962e9f5bb22db9794c6b9f2655
lfemina <lfemina@yandex.ru>
2024-12-14 17:12:35 +0300

    added styles for error page 404

--- src/Router/Missing.jsx
+++ src/Router/Missing.jsx
@@ -1,7 +1,13 @@
+import "./Missing.css";
+
 function Missing() {
     return (
-        <main>
-            <h1>404 Not Found</h1>
+        <main className="not-found">
+            <h1 className="not-found-head1">404 ERROR PAGE</h1>
+            <h2 className="not-found-head2">Страница не найдена</h2>
+            <p className="not-found-prg">К сожалению, эта страница не существует.</p>
+            <p className="not-found-prg">Но мы постоянно развиваемся, может быть в будущем мы это исправим.</p>
+            <a href="/" className="not-found-btn-link">Вернуться на главную страницу</a>
         </main>
     );
 };
